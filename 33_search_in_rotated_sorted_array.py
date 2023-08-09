11255e84dce733380330eed05be90761ef2602cc
Thiha MM <thaminmg@gmail.com>
2023-08-08 17:14:18 -0700

    08/08/2023

---
+++ 33_search_in_rotated_sorted_array.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    def search(self, nums: list[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+
+# nums = [4,5,6,7,0,1,2]
+nums = [5,1,3]
+target = 3
+print(Solution().search(nums, target))
