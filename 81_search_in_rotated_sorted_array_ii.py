6e728f0cfa1948cf63dc614ae27af0a50856b717
Thiha MM <thaminmg@gmail.com>
2023-08-09 23:38:28 -0700

    08/09/23

--- 81_search_in_rotated_sorted_array_ii.py
+++ 81_search_in_rotated_sorted_array_ii.py
@@ -1,28 +1,31 @@
 class Solution:
-
-    def search(self, nums: list[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+    def search(self, nums: list[int], target: int) -> bool:
+        left, right = 0, len(nums) -1
 
         while left <= right:
+            # check duplicates
+            while (left < right and nums[left] == nums[left + 1]):
+                left += 1
+            while (left < right and nums[right] == nums[right - 1]):
+                right -= 1
+
+            # bin search
             mid = (left + right) // 2
             if nums[mid] == target:
-                return mid
+                return True
             
             if nums[left] <= nums[mid]:
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
-
             else:
                 if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
                     right = mid - 1
-        return -1
-
-
-# nums = [4,5,6,7,0,1,2]
-nums = [3, 1]
-target = 1
+        return False
+    
+nums = [1,1,1,1,1,1,1,1,1,13,1,1,1,1,1,1,1,1,1,1,1,1]
+target = 13
 print(Solution().search(nums, target))
