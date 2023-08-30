6aeaabf24fd53c674d8ffb31b923d8ea78547535
thaminmg <thaminmg@gmail.com>
2023-08-29 23:01:54 -0700

    08/29/2023

--- 143_reorder_list.py
+++ 143_reorder_list.py
@@ -1,50 +1,45 @@
 # Definition for singly-linked list.
-
-from typing import Optional
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        slow, fast = head, head.next
+        slow, fast = head, head
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        second = slow.next
-        slow.next = None # disconnect
-        # reversing
+
+        second = slow.next    
+        slow.next = None
+
         prev = None
-        while second:
-            temp = second.next
-            second.next = prev
-            prev = second
-            second = temp
-        # merging
-        first, second = head, prev
-        result = ListNode()
-        while first and first.next:
-            result.next = first
-            result = result.next
-
-
-
+        curr = second
+        
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
     
+        curr1, curr2 = head, prev
+
+        while curr2:
+            temp1, temp2 = curr1.next, curr2.next
+            curr1.next = curr2
+            curr2.next = temp1
+            curr1, curr2 = temp1, temp2
+
+
+
 
         
+            
 
 
-def convert_to_linked_list(input_list):
-    head = None
-    for i in range(len(input_list) - 1, -1, -1):
-        new_head = ListNode(input_list[i], head)
-        head = new_head
-    return head
 
-head = [1,2,3,4]
-print(Solution().reorderList(convert_to_linked_list(head)))
+        
