e02f88d4000b79d1d1deb7db9054f6a9e671d72d
thaminmg <thaminmg@gmail.com>
2023-09-03 00:32:07 -0700

    09/02/2023

--- stack_queues/20_valid_parentheses.py
+++ stack_queues/20_valid_parentheses.py
@@ -34,7 +34,24 @@ def isValid(self, s):
         if i in open_par:
             stack.append(i)
         elif stack and i == bracket_map[stack[-1]]:
-                stack.pop()
+            stack.pop()
         else:
             return False
     return stack == []
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for i in s:
+            if i == '(' or i == '[' or i == '{':
+                stack.append(i)
+            else:
+                if stack:
+                    top = stack[-1]
+                    if i == ')' and top == '(' or i == '}' and top == '{' or i == ']' and top == '[':
+                        stack.pop()
+                    else:
+                        return False
+                else:
+                    return False
+        return False if stack else True
