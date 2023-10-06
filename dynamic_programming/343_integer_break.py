cc3a16058a1910ddc109e61c930b28c43955769b
Thiha MM <thaminmg@gmail.com>
2023-10-06 00:01:11 -0700

    10/05/23

--- dynamic_programming/343_integer_break.py
+++ dynamic_programming/343_integer_break.py
@@ -1,14 +1,25 @@
 class Solution:
-    # math solution using number of threes
+    
     def integerBreak(self, n: int) -> int:
-        if n < 4: return n - 1
+        # DP
+        dp = {1: 1}
 
-        noThree = n // 3
-        res = 3 ** noThree
-        if n % 3 == 1:
-            res //= 3
-            res *= 4
-        elif n % 3 == 2:
-            res *= 2
-        return res
+        for num in range(2, n + 1):
+            dp[num] = 0 if num == n else num
+            for i in range(1, num):
+                val = dp[i] * dp[num - i]
+                dp[num] = max(dp[num], val)
+        return dp[n]
+
+    # math solution using number of threes
+    #     if n < 4: return n - 1
+
+    #     noThree = n // 3
+    #     res = 3 ** noThree
+    #     if n % 3 == 1:
+    #         res //= 3
+    #         res *= 4
+    #     elif n % 3 == 2:
+    #         res *= 2
+    #     return res
         
