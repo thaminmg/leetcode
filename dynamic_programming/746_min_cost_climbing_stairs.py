7e48628bdee56f1e3847139c077bb5c7058b1dd4
Thiha MM <thaminmg@gmail.com>
2023-10-12 21:53:26 -0700

    10/12/23

--- dynamic_programming/746_min_cost_climbing_stairs.py
+++ dynamic_programming/746_min_cost_climbing_stairs.py
@@ -1,12 +1,21 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # n = len(cost)
+        # dp = [0] * n
+
+        # for i in range(n):
+        #     if i < 2:
+        #         dp[i] = cost[i]
+        #     else:
+        #         dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+
+        # return min(dp[-1], dp[-2]) 
         n = len(cost)
-        dp = [0] * n
+        dp = [0] * (n + 1)
 
-        for i in range(n):
-            if i < 2:
-                dp[i] = cost[i]
-            else:
-                dp[i] = cost[i] + min(dp[i - 1], dp[i - 2])
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i-1] + cost[i - 1], dp[i - 2] + cost[i - 2])
 
-        return min(dp[-1], dp[-2])        
+        return dp[n]
+             
+      
