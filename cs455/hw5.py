e74ebdb0dd7a2f466251177d8112f09486809ab4
Thiha MM <thaminmg@gmail.com>
2023-12-11 22:12:22 -0800

    12/11/23

--- cs455/hw5.py
+++ cs455/hw5.py
@@ -1,52 +1,33 @@
+import random
 import time
 
 # Question 1
-
-def compute_transition_function(pattern, m):
-    alphabet = {'0', '1'}
-    TF = [{} for _ in range(m+1)]
-    for state in range(m + 1):
-        for char in alphabet:
-            next_state = 0 if state == m else state + 1
-            while next_state > 0 and char != pattern[next_state - 1]:
-                next_state = TF[next_state - 1].get(char, 0)
-            TF[state][char] = next_state       
-    return TF
-
 def fsa_search(pattern, text):
-    m = len(pattern)
-    n = len(text)
-    TF = compute_transition_function(pattern, m)
+    state = 0
     matches = []
-    state=0
-
-    for i in range(n):
-        state = TF[state].get(text[i], 0)
-        if state == m:
-            matches.append(i - m + 1)
-            state = 0
+    fsa = compute_transition_function(pattern, len(pattern))
+    for i in range(len(text)):
+        state = fsa[state][text[i]]
+        if state == len(pattern):
+            matches.append(i - len(pattern) + 1)
     return matches
 
-text = "00100100101001001"
-pattern = "001"
+def compute_transition_function(pattern, m):
+    fsa = [{'0': 0, '1': 0} for _ in range(m + 1)]
+    for q in range(m+1):
+        for a in ['0', '1']:
+            k = min(m, q+1)
+            while k > 0 and pattern[:k] != pattern[q - k + 1:q] + a:
+                k = k - 1
+            fsa[q][a] = k
+    return fsa
+
+text = "011100111100111001101101110011"
+pattern = "01110011"
+
 print(fsa_search(pattern, text))
 
 # Question 2
-def compute_prefix_function(P, m):
-    pi = [0] * m
-    k = 0
-
-    for q in range(1, m):
-        while k > 0 and P[q] != P[k]:
-           k = pi[k - 1]
-
-        if P[q] == P[k]:
-            k += 1
-
-        pi[q] = k
-
-    return pi
-
 def kmp_search(pattern, text):
     n = len(text)
     m = len(pattern)
@@ -56,19 +37,29 @@ def kmp_search(pattern, text):
     for i in range(n):
         while q > 0 and text[i] != pattern[q]:
             q = pi[q - 1]
-
         if text[i] == pattern[q]:
             q += 1
-
         if q == m:
             matches.append(i - m + 1)
             q = pi[q - 1]
     return matches
 
-text = "00100100101001001"
-pattern = "001"
+def compute_prefix_function(pattern, m):
+    pi = [0] * m
+    k = 0
+    for q in range(1, m):
+        while k > 0 and pattern[q] != pattern[k]:
+           k = pi[k - 1]
+        if pattern[q] == pattern[k]:
+            k += 1
+        pi[q] = k
+    return pi
+
+text = "011100111100111001101101110011"
+pattern = "01110011"
 print(kmp_search(pattern, text))
 
+# Question 3
 def evaluate_build_time(algorithm, patterns):
     results = []
     for pattern in patterns:
@@ -79,78 +70,73 @@ def evaluate_build_time(algorithm, patterns):
         results.append(elapsed_time)
     return results
 
+def generate_random_pattern(length):
+    return ''.join(random.choice(['0', '1']) for _ in range(length))
 
+pattern_lengths = [10, 100, 1000] #10000
+patterns = [generate_random_pattern(length) for length in pattern_lengths]
+q32 = [
+    ''.join(str(bit) for bit in ([0, 1, 0] * 3) + [0, 0, 0, 0]) * 10,
+    ''.join(str(bit) for bit in ([0, 1, 0] * 3) + [0, 0, 0, 0]) * 100,
+    ''.join(str(bit) for bit in ([0, 1, 0] * 10) + [0, 0, 0, 0]) * 100,
+    ''.join(str(bit) for bit in ([0, 1, 0] * 10) + [0, 0, 0, 0]) * 1000
+    ]
+patterns.extend(q32)
 
-# # Question 3
-patterns_q3 = [
-    ''.join(str(bit) for bit in [0, 1] * 5),
-    ''.join(str(bit) for bit in [0, 1] * 50),
-    ''.join(str(bit) for bit in [0, 1] * 500),
-    ''.join(str(bit) for bit in [0, 1] * 5000),
-    ''.join(str(bit) for bit in ([0, 1, 0] * 3) + [0, 0] * 4),
-    ''.join(str(bit) for bit in ([0, 1, 0] * 3) + [0, 0] * 99),
-    ''.join(str(bit) for bit in ([0, 1, 0] * 10) + [0, 0] * 99),
-    ''.join(str(bit) for bit in ([0, 1, 0] * 10) + [0, 0] * 999),
-]
-
-results_q3_fsa_build = evaluate_build_time(compute_transition_function, patterns_q3)
-results_q3_kmp_build = evaluate_build_time(compute_prefix_function, patterns_q3)
+results_fsa_build = evaluate_build_time(compute_transition_function, patterns)
+results_kmp_build = evaluate_build_time(compute_prefix_function, patterns)
 
 # Display results
-print("Results for FSA Build Time:")
-print("| Pattern Length  | 10     | 50    | 500   | 5000  | (010^3 0000)^10 | (010^3 0000)^100 | (010^10 0000)^100 | (010^10 0000)^1000 |")
-print("|-----------------|--------|-------|-------|-------|-----------------|------------------|---------------------|----------------------|")
-print(f"| Build Time      | {results_q3_fsa_build[0]:.4f} | {results_q3_fsa_build[1]:.4f} | {results_q3_fsa_build[2]:.4f} | {results_q3_fsa_build[3]:.4f} | {results_q3_fsa_build[4]:.4f}       | {results_q3_fsa_build[5]:.4f}         | {results_q3_fsa_build[6]:.4f}            | {results_q3_fsa_build[7]:.4f}             |")
+print("Results for Build Time:")
+print("|----------------------------------------------------------------|")
+print("|   Pattern Length   |  FSA Build Time (s)  | KMP Build Time (s) |")
+print("|--------------------|----------------------|--------------------|")
+print(f"| 10                 |     {results_fsa_build[0]:.10f}     |    {results_kmp_build[0]:.10f}    |")
+print(f"| 100                |     {results_fsa_build[1]:.10f}     |    {results_kmp_build[1]:.10f}    |")
+print(f"| 1000               |     {results_fsa_build[2]:.10f}     |    {results_kmp_build[2]:.10f}    |")
+# print(f"| 10000              |    {results_fsa_build[3]:.10f}    |    {results_kmp_build[3]:.10f}    |")
+print(f"| (010^3 0000)^10    |     {results_fsa_build[3]:.10f}     |    {results_kmp_build[3]:.10f}    |")
+print(f"| (010^3 0000)^100   |     {results_fsa_build[4]:.10f}     |    {results_kmp_build[4]:.10f}    |")
+print(f"| (010^10 0000)^100  |     {results_fsa_build[5]:.10f}     |    {results_kmp_build[5]:.10f}    |")
+print(f"| (010^10 0000)^1000 |    {results_fsa_build[6]:.10f}    |    {results_kmp_build[6]:.10f}    |")
+print("|----------------------------------------------------------------|")
+print()
 
-print("\nResults for KMP Build Time:")
-print("| Pattern Length  | 10     | 50    | 500   | 5000  | (010^3 0000)^10 | (010^3 0000)^100 | (010^10 0000)^100 | (010^10 0000)^1000 |")
-print("|-----------------|--------|-------|-------|-------|-----------------|------------------|---------------------|----------------------|")
-print(f"| Build Time      | {results_q3_kmp_build[0]:.4f} | {results_q3_kmp_build[1]:.4f} | {results_q3_kmp_build[2]:.4f} | {results_q3_kmp_build[3]:.4f} | {results_q3_kmp_build[4]:.4f}       | {results_q3_kmp_build[5]:.4f}         | {results_q3_kmp_build[6]:.4f}            | {results_q3_kmp_build[7]:.4f}             |")
-
-
-# # Question 4
-
-def evaluate_match_time(algorithm, text, patterns):
-    results = []
-    for pattern in patterns:
-        # algorithm_data = algorithm(pattern, text)
-        start_time = time.time()
-        # algorithm_data(text, pattern)
-        algorithm(pattern, text)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        results.append(elapsed_time)
-    return results
+# Question 4
+def evaluate_match_time(algorithm, pattern, text):
+    start_time = time.time()
+    algorithm(pattern, text)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    return  elapsed_time
 
 patterns_q4 = ['01110011', '10000000']
-n_values_q4 = [100, 1000]
+values = [100, 1000]
 
-results_q4_fsa_match = []
-results_q4_kmp_match = []
+results_fsa_match = []
+results_kmp_match = []
 
 for pattern in patterns_q4:
-    for n in n_values_q4:
-        text = ''.join(str(bit) for bit in [0, 1] * (n // 2))
-        positions = [i for i in range(0, n, n // 20)][:5]
+    for n in values:
+        text = generate_random_pattern(n)
+        positions = [random.randint(0, n) for _ in range(5)]
         
         for pos in positions:
             text = text[:pos] + pattern + text[pos + len(pattern):]
         
-        result_fsa = evaluate_match_time(fsa_search, text, [pattern])
-        result_kmp = evaluate_match_time(kmp_search, text, [pattern])
+        result_fsa = evaluate_match_time(fsa_search, pattern, text)
+        result_kmp = evaluate_match_time(kmp_search, pattern, text)
         
-        results_q4_fsa_match.append(result_fsa[0])
-        results_q4_kmp_match.append(result_kmp[0])
+        results_fsa_match.append(result_fsa)
+        results_kmp_match.append(result_kmp)
 
-
-print("\nResults for FSA Match Time:")
-print("| Pattern | Text Length | 100  | 1000 |")
-print("|---------|-------------|------|------|")
-print(f"| 01110011| Match Time  | {results_q4_fsa_match[0]:.4f} | {results_q4_fsa_match[1]:.4f} |")
-print(f"| 10000000| Match Time  | {results_q4_fsa_match[2]:.4f} | {results_q4_fsa_match[3]:.4f} |")
-
-print("\nResults for KMP Match Time:")
-print("| Pattern | Text Length | 100  | 1000 |")
-print("|---------|-------------|------|------|")
-print(f"| 01110011| Match Time  | {results_q4_kmp_match[0]:.4f} | {results_q4_kmp_match[1]:.4f} |")
-print(f"| 10000000| Match Time  | {results_q4_kmp_match[2]:.4f} | {results_q4_kmp_match[3]:.4f} |")
+print("Results for Match Time:")
+print("|--------------------------------------------------------------------------------------------------------|")
+print("|                    | FSA Match Time (s)                      | KMP Match Time (s)                      |")
+print("|--------------------|--------------------|--------------------|--------------------|--------------------|")
+print("|   Pattern Length   | 100                | 1000               | 100                | 1000               |")
+print("|--------------------|--------------------|--------------------|--------------------|--------------------|")
+print(f"| 01110011           |    {results_fsa_match[0]:.10f}    |    {results_fsa_match[1]:.10f}    |    {results_kmp_match[0]:.10f}    |    {results_kmp_match[1]:.10f}    |")
+print(f"| 10000000           |    {results_fsa_match[2]:.10f}    |    {results_fsa_match[3]:.10f}    |    {results_kmp_match[2]:.10f}    |    {results_kmp_match[3]:.10f}    |")
+print("|--------------------------------------------------------------------------------------------------------|")
+print()
