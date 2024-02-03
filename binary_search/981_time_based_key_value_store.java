7f123226a037122d59a307e765a326b7acdd843b
Thiha MM <thaminmg@gmail.com>
2024-02-03 00:44:06 -0800

    2/2/24

--- binary_search/981_time_based_key_value_store.java
+++ binary_search/981_time_based_key_value_store.java
@@ -1,64 +1,36 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
 class TimeMap {
 
-    private HashMap<String, ArrayList<HashMap<Integer, String>>> mappings;
+    private HashMap<String, ArrayList<Pair<Integer, String>>> mappings;
 
     public TimeMap() {
-        mappings = new HashMap<String, ArrayList<HashMap<Integer, String>>>();
+        mappings = new HashMap<String, ArrayList<Pair<Integer, String>>>();
     }
     
     public void set(String key, String value, int timestamp) {
-        HashMap<Integer, String> subMappings = new HashMap<Integer, String>();
-        subMappings.put(timestamp, value);
-        ArrayList<HashMap<Integer, String>> lst;
-        if (mappings.containsKey(key)) {
-            lst = mappings.get(key);
-        } else {
-            lst = new ArrayList<HashMap<Integer, String>>();
-        }
-        lst.add(subMappings);
-        mappings.put(key, lst);
+      
+        if (!mappings.containsKey(key)) {
+            mappings.put(key, new ArrayList());
+        } 
+        mappings.get(key).add(new Pair(timestamp, value));        
     }
     
     public String get(String key, int timestamp) {
-        ArrayList<HashMap<Integer, String>> lst = mappings.get(key);
-        int n = lst.size();
+        if (!mappings.containsKey(key)) return "";
+        if (timestamp < mappings.get(key).get(0).getKey()) return "";
 
-        if (n == 0) return "";
-
-        int left = 0, right = n - 1;
-        HashMap<Integer, String> curr;
+        int n = mappings.get(key).size();
+        int left = 0, right = n;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            curr = lst.get(mid);
-            
-            ArrayList<Integer> keys = new ArrayList<>();
-            ArrayList<String> vals = new ArrayList<>();
 
-            for (Entry<Integer, String> entry : curr.entrySet()) {
-                keys.add(entry.getKey());
-                vals.add(entry.getValue());
-            }
-
-            if (keys.get(0) == timestamp) {
-                return vals.get(0);
-            } else if (keys.get(0) > timestamp) {
-                right = mid;
-            } else {
+            if (mappings.get(key).get(mid).getKey() <= timestamp) {
                 left = mid + 1;
+            } else{
+                right = mid;
             }
         }
-        ArrayList<Integer> keys = new ArrayList<>();
-        ArrayList<String> vals = new ArrayList<>();
-
-        for (Entry<Integer, String> entry : lst.get(left).entrySet()) {
-            keys.add(entry.getKey());
-            vals.add(entry.getValue());
-        }
-        return vals.get(0);
+        if (right == 0) return "";
+        return mappings.get(key).get(right - 1).getValue();
     }
 }
 
