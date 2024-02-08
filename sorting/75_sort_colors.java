8b5a3c16fe2490ad206b5dcd058283b307c590dc
Thiha MM <thaminmg@gmail.com>
2024-02-07 21:54:01 -0800

    2/7/24

---
+++ sorting/75_sort_colors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int minIndex;
+        for (int i = 0; i < nums.length; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = temp;
+        }
+    }
+}
