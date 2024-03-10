b3ab01152b7b55ce8e468a4b277a71c6de2e6238
Thiha MM <thaminmg@gmail.com>
2024-03-09 20:23:23 -0800

    3/9/24

---
+++ two_pointer/167_two_sum_ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        int left = 0, right = numbers.length - 1;
+        
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        
+        return res;
+    }
+}
