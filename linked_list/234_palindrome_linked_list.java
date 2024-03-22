de7fddf96dff45166a1cf02fa55b5ad586b06626
Thiha MM <thaminmg@gmail.com>
2024-03-21 22:23:55 -0700

    3/21/24

---
+++ linked_list/234_palindrome_linked_list.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null) return true;
+
+        ListNode firstEnd = endOfFirstHalf(head);
+        ListNode secondStart = reverseSecondHalf(firstEnd.next);
+
+        boolean result = true;
+        ListNode p1 = head;
+        ListNode p2 = secondStart;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        firstEnd.next = reverseSecondHalf(secondStart);
+        return result;
+    }
+
+    ListNode reverseSecondHalf(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+    ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}s
