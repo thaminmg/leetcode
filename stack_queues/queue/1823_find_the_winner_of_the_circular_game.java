ebbe764af4cf4db87f8aabfa7a4348e8605c6322
Thiha MM <thaminmg@gmail.com>
2024-07-07 23:24:49 -0700

    7/7/24

---
+++ stack_queues/queue/1823_find_the_winner_of_the_circular_game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findTheWinner(int n, int k) {
+        Queue<Integer> queue = new LinkedList();
+
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+
+        while (queue.size() > 1) {
+            for (int i = 0; i < k - 1; i++) {
+                queue.add(queue.remove());
+            }
+            queue.remove();
+        }
+        return queue.peek();
+    }
+}
